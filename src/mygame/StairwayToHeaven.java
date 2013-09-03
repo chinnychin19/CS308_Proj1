2ef0455f6508421e16fcc31b99a7722224e23ecc
Chinmay Patwardhan <chinnychin19@gmail.com>
2013-09-03 02:30:15 -0400

    Organized gameplay into stages

--- src/mygame/StairwayToHeaven.java
+++ src/mygame/StairwayToHeaven.java
@@ -1,8 +1,5 @@
 package mygame;
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 import jgame.*;
 import jgame.platform.*;
@@ -25,43 +22,77 @@ public class StairwayToHeaven extends StdGame {
 		return player;
 	}
 	
-	public void initCanvas() {						
+	public void initCanvas() {
+		print("initCanvas");
 		setCanvasSettings(Constants.nrtilesx, Constants.nrtilesy,
 				Constants.tilex, Constants.tiley, 
 				null, 
 				new JGColor(Constants.bgcolorR, Constants.bgcolorG, Constants.bgcolorB), 
 				null);
 	}
+
 	public void initGame() {
 		defineMedia("mygame.tbl");
-		if (isMidlet()) {
-			setFrameRate(20,1);
-			setGameSpeed(2.0);
-		} else {
-			setFrameRate(45,1);
+		setFrameRate(45,1);
+		setGameState("Title");
+	}
+
+
+	/** Called when the Title state is entered. */
+	public void startTitle() {
+	}
+
+	public void paintFrameTitle() {
+		double firstTextHeight = 40;
+		double interval = 25;
+		drawString("\"BAT OUT OF HELL\"", viewWidth()/2, firstTextHeight, 0);
+		drawString("Hell is chasing you!", viewWidth()/2, firstTextHeight + interval, 0);
+		drawString("Run away and pick up building blocks!", viewWidth()/2, firstTextHeight + 2*interval, 0);
+		drawString("These will help you later... ;)", viewWidth()/2, firstTextHeight + 3*interval, 0);
+		drawString("(Press spacebar to begin)", viewWidth()/2, firstTextHeight + 4*interval, 0);
+	}
+
+	public void doFrameTitle() {
+		if (getKey(' ')) {
+			clearKey(' ');
+			// Set both StartGame and InGame states simultaneously.
+			setGameState("StartGame");
+			addGameState("InGame");
+			// set a timer to remove the StartGame state after a few seconds,
+			new JGTimer(70, true, "StartGame") {
+				public void alarm() {
+					removeGameState("StartGame");
+				}
+			};
 		}
-		setPFSize(Constants.PF_HORIZONTAL_TILES_PART1, Constants.PF_VERTICAL_TILES_PART1);
-		frames = 0;
-		setGameState("Scrolling");
 	}
-	
-	public void startScrolling() {
-		removeObjects(null,0);
-		print("started scrolling mode");
+
+	/** The StartGame state is just for displaying a start message. */
+	public void paintFrameStartGame() {
+		drawString("GO!",viewWidth()/2,90,0);
 	}
-	
+
+	/** Called once when game goes into the InGame state. */
+	public void startInGame() {
+		print("startInGame()");
+	}
+		
 	public void print(Object o) {
 		System.out.println(o.toString());
 	}
 	
 	public void initNewLife() {
+		print("initNewLife()");
 		removeObjects(null,0);
+		setPFSize(Constants.PF_HORIZONTAL_TILES_PART1, Constants.PF_VERTICAL_TILES_PART1);
 		player = new Player(this, Constants.PLAYER_START_X, Constants.PLAYER_START_Y, Constants.PLAYER_SPEED);
 		darkness = new Darkness(this, Constants.PLAYER_START_X-viewWidth(), Constants.TOP_MARGIN, 
 				viewWidth()/2, viewHeight());
+		frames = 0;
 		makeStage();
 	}
-	private void makeStage() {
+	
+	public void makeStage() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for (char c: Constants.STAGE_STRING.toCharArray()) {
 			list.add(Integer.parseInt(""+c));
@@ -78,10 +109,16 @@ public class StairwayToHeaven extends StdGame {
 	}
 	
 	public void startGameOver() {
+		print("startGameOver");
 		removeObjects(null,0); 
 	}
 	
 	public void doFrameInGame() {
+		if (getKey('Z')) {
+			clearKey('Z');
+			//TODO: cheat code
+		}
+		
 		setViewOffset((int)player.x, (int)player.y, true); //pans screen with player
 		frames++;
 		moveObjects();
@@ -91,7 +128,7 @@ public class StairwayToHeaven extends StdGame {
 		checkCollision(Constants.PLATFORM_CID, Constants.PLAYER_CID);
 		
 		if (player.x > this.pfWidth() - JGObject.tilewidth ) {
-			System.exit(0); //TODO: set new stage with text first
+			setGameState("Stairs");
 		} else if (darkness.x+darkness.getWidth() > player.x) {
 			lifeLost(); //caught by darkness
 		} else if (player.y > viewHeight()) {
